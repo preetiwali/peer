13cb1fee28bb031034e3d56002f2d4e44e0f4485
Preeti <preetiwali212@gmail.com>
2021-04-21 12:16:35 +0400

    webrtc peerp to peer video chat and share screen

---
+++ index.js
@@ -0,0 +1,15 @@
+const ejs = require ('ejs');
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('main')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
